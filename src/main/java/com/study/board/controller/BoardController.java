96debb0a5f1e036d859c42677b31a4140a0fece6
kyung suk <dno06117@naver.com>
2024-03-20 22:54:05 +0900

    한코딩-7 기존 @ResponseBody를 통한 메세지 보여주는 거 말고 @GetMapping을 통해 view로 이동하는걸 배움. 또한 해당 view(html 파일)로 return을 통해서 접근

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
-    @GetMapping("/")
-    @ResponseBody // return에 해당하는 "hello world"를 띄워줄 수 있게 해주는 annotation
-    public String main(){
 
-        return "hello world";
+
+    @GetMapping("/board/write") // 어떤 url로 접근할것인지를 지정해주는 애노테이션 // localhost:8080/board/write
+    public String BoardWriteForm(){
+
+        return "boardwrite"; // "" 사이에 들어가는것은 어떤 html 파일(view 파일)로 들어갈 것인지를 알려주는것
     }
 }
